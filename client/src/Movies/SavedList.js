18d594a9373a6a6993d59f6ecb34cdf76d24ad3c
Ikechukwu Eze <iykekings36@gmail.com>
2019-06-12 01:19:11 +0100

    Turn Saved Movie Links into NavLinks

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -11,9 +11,9 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <Link to={`/movies/${movie.id}`} className="saved-movie">
+          <NavLink to={`/movies/${movie.id}`} activeClassName="saved-active">
             {movie.title}
-          </Link>
+          </NavLink>
         ))}
         <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
           <div className="home-button">Home</div>
